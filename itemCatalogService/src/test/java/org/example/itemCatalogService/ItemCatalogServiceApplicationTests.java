9a58ea61a387f867d18c109e9697471bd68b463b
GU <badegowdausha@gmail.com>
2020-09-02 22:26:16 +0530

    itemCatalogService

---
+++ itemCatalogService/src/test/java/org/example/itemCatalogService/ItemCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.itemCatalogService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
